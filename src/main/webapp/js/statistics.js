99d986cee5a74f369602c344144d693088eb6aa6
CihanErenler <cihanerenler@outlook.com>
2021-04-08 18:22:19 +0300

    Statistics page is cooler now

--- src/main/webapp/js/statistics.js
+++ src/main/webapp/js/statistics.js
@@ -5,7 +5,11 @@ questions.forEach((item, i) => {
 	
 	item.querySelectorAll(".chart").forEach((b, index) => {
 		b.querySelectorAll(".chart-wrap").forEach((w, wi) => {
-			w.children[2].textContent = val[wi]
+			w.children[2].querySelector(".num").textContent = val[wi]
+			const n = +w.children[2].querySelector(".num").textContent;
+			if(n === 0){
+				w.children[2].querySelector(".num").nextElementSibling.remove();
+			}
 			w.children[1].children[0].style.width = calculate(parseInt(val[wi]), questions.length)+"%";
 		})
 	})
