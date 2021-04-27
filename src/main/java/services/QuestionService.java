bd2b323d89efcd4352a48284be76f84575d293a8
rhexa <akun.kripitit@gmail.com>
2021-04-27 18:56:46 +0300

    questionservice update and updateque servlet

--- src/main/java/services/QuestionService.java
+++ src/main/java/services/QuestionService.java
@@ -8,8 +8,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
@@ -22,13 +25,25 @@ public class QuestionService {
 //	private
 	@Path("/add")
 	public Response addQuestion() {
+		
 		return Response.ok().build();
 	}
 
 //	private
+	@POST
 	@Path("/update")
-	public Response updateQuestion() {
-		return Response.ok().build();
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateQuestion(Question q) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Question que=em.find(Question.class, q.getId());
+		if (que != null) {
+			em.merge(q);
+		}
+		em.getTransaction().commit();
+		
+		return readAllQuestion();
 	}
 
 //	private	
@@ -38,8 +53,11 @@ public class QuestionService {
 	}
 
 //	public
-	@Path("/read")
-	public Response readQuestion() {
+	@GET
+	@Path("/read/{id}")
+	public Response readQuestion(@PathParam("id") String id) {
+		EntityManager em = emf.createEntityManager();
+		
 		return Response.ok().build();
 	}
 
